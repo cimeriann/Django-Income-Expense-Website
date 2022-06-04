34960d5a82c9524b6ad51e0aad090164c440c06b
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2022-06-04 19:02:58 +0100

    fixed data leaks

---
+++ expenseswebsite/env.bat
@@ -0,0 +1,4 @@
+set DB_NAME=incomeexpensesdb
+set DB_USER=postgres
+set DB_USER_PASSWORD=testpassword
+set DB_HOST=localhost
--- expenseswebsite/expenseswebsite/settings.py
+++ expenseswebsite/expenseswebsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +77,10 @@ WSGI_APPLICATION = 'expenseswebsite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'incomeexpensesdb',
-        'USER': 'postgres',
-        'PASSWORD': 'testpassword',
-        'HOST': 'localhost',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_USER_PASSWORD'),
+        'HOST': os.environ.get('DB_USER'),
     }
 }
 

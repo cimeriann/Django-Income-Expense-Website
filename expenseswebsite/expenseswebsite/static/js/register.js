e5d0bb1c04d252683fe8841e8df9c5b465a2f32b
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2022-06-17 22:20:40 +0100

    Fixes

--- expenseswebsite/expenseswebsite/static/js/register.js
+++ expenseswebsite/expenseswebsite/static/js/register.js
@@ -2,5 +2,17 @@ const usernameField = document.querySelector("#usernameField");
 
 usernameField.addEventListener("keyup", (e) => {
   const usernameVal = e.target.value;
-  console.log('usernameVal', usernameVal);
-})
+
+  if (usernameVal.length > 0) {
+      fetch("/authentication/validate-username", {
+        body: JSON.stringify({ username: usernameVal }),
+        method: "POST",
+      }).then((res) => res.json()).then((data) => {
+      console.log('data', data);
+      if(data.username_error){
+        usernameField.classList.add("is-invalid")
+      }
+    });
+  }
+
+});

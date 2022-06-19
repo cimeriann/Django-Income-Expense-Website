4618982453b52127d9b0856591df481cf392b053
Olayinka Adebisi <adebisiolayinka30@gmail.com>
2022-06-19 22:15:04 +0100

    Ugh!

--- expenseswebsite/authentication/urls.py
+++ expenseswebsite/authentication/urls.py
@@ -1,8 +1,9 @@
 from urllib.parse import urlparse
-from . import views
+from .views import RegistrationView, UsernameValidationView, EmailValidationView
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
-    path('register/', views.RegistrationView.as_view(), name='register'),
-    path('validate-username/',csrf_exempt(views.UsernameValidationView.as_view()), name='validate-username')
+    path('register/', RegistrationView.as_view(), name='register'),
+    path('validate-username/', csrf_exempt(UsernameValidationView.as_view()), name='validate-username'),
+    path('validate-email/', csrf_exempt(EmailValidationView.as_view()), name='validate_email')
 ]
